adc310043ff1d8ff036c99d13a10dba16b8ac72c
Peter Reincke <peter.reincke@adesso.de>
2023-02-09 13:22:11 +0100

    MenuItem now a class with code and text - code will be returned on selection + some minor bugfix

---
+++ Models/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentConsoleMenu.Models
+{
+    public class MenuItem
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+
+        public MenuItem(string code, string text)
+        {
+            Code = code;
+            Text = text;
+        }
+
+    }
+}
